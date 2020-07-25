601b7f51a3a07c67ce69472d33aec89133ce8896
millenamoschella@gmail.com <millenamoschella@users.noreply.github.com>
2020-07-25 17:22:53 -0300

    Inserindo seeds

---
+++ app/Empresa.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+
+class Empresa extends Model
+{
+    protected $fillable = [
+        'nome', 
+        'endereco', 
+        'descricao', 
+        'coordenadas'
+    ];
+}
+
