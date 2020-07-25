bc89ee8042f77d757fea8c1f9c0178807cf8ae34
millenamoschella@gmail.com <millenamoschella@users.noreply.github.com>
2020-07-25 18:33:06 -0300

    Inserindo um painel de admin

---
+++ resources/views/admin/admin.blade.php
@@ -0,0 +1,11 @@
+@extends('estrutura.layout')
+
+
+  @section('h1')
+  <h1 class="text-center text-white my-2 p-2 text-uppercase">Admin</h1>
+  @endsection
+
+
+@section('content')
+    
+@endsection
